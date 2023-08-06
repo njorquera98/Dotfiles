63628a1178f8c2f7807022443e439692e0f9222c
njorquera98 <njorquera98.dev@gmail.com>
2023-08-06 17:21:31 -0400

    Add Nvim zsh Wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require "wezterm"
+
+return {
+  --font = wezterm.font "Hack",
+  font_size = 13,
+  color_scheme = 'Monokai Remastered',
+  keys = {
+    {
+      key = "k",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.DisableDefaultAssignment,
+    }
+  },
+  window_background_opacity = .95,
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
+  scrollback_lines = 5000,
+}
