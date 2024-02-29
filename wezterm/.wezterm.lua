925dbadde131f933d067ed45498fe671d131d1f5
njorquera98 <njorquera98.dev@gmail.com>
2024-02-29 00:01:48 -0300

    new dotfiles

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("Hack Nerd Font"),
+	font_size = 12,
+	color_scheme = "Monokai Soda",
+	keys = {
+		{
+			key = "k",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+	},
+	window_background_opacity = 0.95,
+	hide_tab_bar_if_only_one_tab = true,
+	scrollback_lines = 5000,
+}
