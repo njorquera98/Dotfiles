ba93f19bdcfcacdbe9c31f6df9c3365f681f8b7f
njorquera98 <njorquera98.dev@gmail.com>
2024-04-14 21:42:18 -0400

    Add Rest LazyGit

--- LazyVim/.config/nvim/lua/plugins/plugins.lua
+++ LazyVim/.config/nvim/lua/plugins/plugins.lua
@@ -12,9 +12,33 @@ return {
 	},
 
 	{
-		"jesseduffield/lazygit",
-		requires = {
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+
+	{
+		"rest-nvim/rest.nvim",
+		ft = "http",
+		config = function()
+			require("rest-nvim").setup({})
+		end,
+	},
+
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
 			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
 	},
 }
