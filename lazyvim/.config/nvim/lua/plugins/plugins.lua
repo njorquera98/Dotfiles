c863ee5670001cad7b336529b534fcaab1fef8d1
njorquera98 <njorquera98.dev@gmail.com>
2024-05-13 16:36:27 -0400

    Update rest-nvim
    space \ rr

--- lazyvim/.config/nvim/lua/plugins/plugins.lua
+++ lazyvim/.config/nvim/lua/plugins/plugins.lua
@@ -21,7 +21,20 @@ return {
 		"rest-nvim/rest.nvim",
 		ft = "http",
 		config = function()
-			require("rest-nvim").setup({})
+			require("rest-nvim").setup({
+				keybinds = {
+					{
+						"<localleader>rr",
+						"<cmd>Rest run<cr>",
+						"Run request under the cursor",
+					},
+					{
+						"<localleader>rl",
+						"<cmd>Rest run last<cr>",
+						"Re-run latest request",
+					},
+				},
+			})
 		end,
 	},
 
