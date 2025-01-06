682bd90c6ee5d908253a99e4cab30f4e1dbb8535
njorquera98 <njorquera98.dev@gmail.com>
2025-01-05 21:41:01 -0300

    update rest-nvim

--- lazyvim/.config/nvim/lua/plugins/plugins.lua
+++ lazyvim/.config/nvim/lua/plugins/plugins.lua
@@ -13,30 +13,11 @@ return {
 	},
 
 	{
-		"vhyrro/luarocks.nvim",
-		priority = 1000,
-		config = true,
-	},
-
-	{
 		"rest-nvim/rest.nvim",
-		ft = "http",
-		config = function()
-			require("rest-nvim").setup({
-				keybinds = {
-					{
-						"<localleader>rr",
-						"<cmd>Rest run<cr>",
-						"Run request under the cursor",
-					},
-					{
-						"<localleader>rl",
-						"<cmd>Rest run last<cr>",
-						"Re-run latest request",
-					},
-				},
-			})
-		end,
+		lazy = false,
+		keys = {
+			{ "<localleader>rr", "<cmd>Rest run<cr>", "Run request under the cursor" },
+		},
 	},
 
 	{
